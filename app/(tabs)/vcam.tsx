dd2fcf24e799d157394e52a85af515a63c5a317e
darshg321 <darshg321@gmail.com>
2025-09-13 16:01:40 -0400

    viro?

--- app/(tabs)/vcam.tsx
+++ app/(tabs)/vcam.tsx
@@ -1,31 +1,31 @@
-// App.tsx
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import ExpoAROverlay from '@/lib/ExpoAROverlay';
-
-export default function Vcam() {
-    const [facing, setFacing] = useState<CameraType>('back');
-    const [permission, requestPermission] = useCameraPermissions();
-
-    if (!permission) return <View style={styles.container} />;
-    if (!permission.granted) {
-        requestPermission();
-        return <View style={styles.container} />;
-    }
-
-    return (
-        <View style={styles.container}>
-            <CameraView style={StyleSheet.absoluteFill} facing={facing}>
-                <ExpoAROverlay blobSource={require('../../assets/flower.png')} />
-            </CameraView>
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'black',
-    },
-});
+// // App.tsx
+// import React, { useState } from 'react';
+// import { StyleSheet, View } from 'react-native';
+// import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+// import ExpoAROverlay from '@/lib/ExpoAROverlay';
+//
+// export default function Vcam() {
+//     const [facing, setFacing] = useState<CameraType>('back');
+//     const [permission, requestPermission] = useCameraPermissions();
+//
+//     if (!permission) return <View style={styles.container} />;
+//     if (!permission.granted) {
+//         requestPermission();
+//         return <View style={styles.container} />;
+//     }
+//
+//     return (
+//         <View style={styles.container}>
+//             <CameraView style={StyleSheet.absoluteFill} facing={facing}>
+//                 <ExpoAROverlay blobSource={require('../../assets/flower.png')} />
+//             </CameraView>
+//         </View>
+//     );
+// }
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: 'black',
+//     },
+// });
