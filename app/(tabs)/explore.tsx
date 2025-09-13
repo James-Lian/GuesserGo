873658a568af44baf83e15f2a195ce23237fe0bd
James-Lian <james-lian@outlook.com>
2025-09-13 09:36:36 -0400

    set up explore tab

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,8 +3,25 @@ import { View } from "react-native";
 import { useFocusEffect } from "expo-router";
 import * as Location from 'expo-location';
 
-export async function requestLocationPermissions() {
-
+export function requestPermissions(locationPermissions: null | boolean, setLocationPermissions: React.Dispatch<React.SetStateAction<boolean | null>>) {
+    // immediately executed async function
+    (async () => {
+        if (locationPermissions !== false) {
+            let { granted, canAskAgain } = await Location.requestForegroundPermissionsAsync();
+            if (granted === false) {
+                console.log("Permission to access location was denied");
+                if (canAskAgain === false) {
+                    setLocationPermissions(false);
+                } else {
+                    setLocationPermissions(null);
+                }
+                return { granted, canAskAgain };
+            }
+            setLocationPermissions(true);
+            return { granted, canAskAgain };
+        }
+        return { granted: true, canAskAgain: true };
+    })();
 }
 
 export default function Explore() {
