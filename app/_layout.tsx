656597a68ed21c33311b879720a19baab3c5c650
darshg321 <darshg321@gmail.com>
2025-09-13 08:31:41 -0400

    start

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,5 @@
-import "../global.css"
-
-import { useEffect } from "react";
-import { SplashScreen, Stack } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-    useEffect(() => {
-        SplashScreen.hide();
-    }, []);
-
-    return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />    
-        </Stack>
-    );
+  return <Stack />;
 }
