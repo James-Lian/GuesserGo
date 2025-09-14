edaed926e58c2bbd8f2d1de57bbeead2776bd664
BrianZhao10 <brianzhaoptimal@gmail.com>
2025-09-14 07:49:00 -0400

    Running Renaming

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,6 +1,7 @@
 // satellite image of somewhere between x km
 // user has to get to the location and take a picture which is x% similar
 // geoguessr style rounds and points system, micro scale
+// Guessr GO
 
 import React, { useRef, useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Modal, Pressable, Text, Image, Alert } from 'react-native';
